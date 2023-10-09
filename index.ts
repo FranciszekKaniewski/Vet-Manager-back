c246476738dc8e4311f584b9ba3f914f3d04ed86
FranciszekKaniewski <fkaniewski.web.com>
2023-10-09 11:06:36 +0200

    Basic setup, README added

---
+++ index.ts
@@ -0,0 +1,34 @@
+import * as express from 'express'
+import 'express-async-errors'
+import {Application} from "express";
+import * as cors from 'cors'
+
+
+export class App{
+    private app: Application;
+
+    constructor() {
+        this.configApp();
+        this.setRoutes();
+        this.run();
+    }
+
+    private configApp() {
+        this.app = express();
+        this.app.use(cors({origin:'http://localhost:3000'}))
+
+        this.app.use(express.json())
+    }
+
+    private setRoutes() {
+
+    }
+
+    private run() {
+        this.app.listen(3001,'localhost',()=>{
+            console.log('Listening on http://localhost:3001/')
+        })
+    }
+}
+
+new App();
