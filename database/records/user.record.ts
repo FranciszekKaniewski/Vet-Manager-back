2772c49dc6eb8fdada43b92b90e4849c36f75cdc
FranciszekKaniewski <fkaniewski.web.com>
2023-10-21 16:43:23 +0200

    getOneByEmail fn added to user.record.ts

--- database/records/user.record.ts
+++ database/records/user.record.ts
@@ -57,6 +57,14 @@ export class UserRecord implements User{
         return results[0] ? new UserRecord(results[0]) : null;
     }
 
+    public static async getOneByEmail(email:string):Promise<User|null>{
+        const [results] = await pool.execute("SELECT * FROM `users` WHERE email = :email",{
+            email:email,
+        }) as UserResult
+
+        return results[0] ? new UserRecord(results[0]) : null;
+    }
+
     public async addOne():Promise<User>{
         const allEmails:string[] = ((await UserRecord.getAll()).map(e=>e.email));
         if(allEmails.filter(email=>email === this.email).length) throw new Error("This email is already taken.");
