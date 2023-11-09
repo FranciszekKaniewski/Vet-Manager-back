317a7e5ce03e240897cb44bb024b6229883c447f
FranciszekKaniewski <fkaniewski.web.com>
2023-11-09 17:31:03 +0100

    Database test change

--- database/db.test.ts
+++ database/db.test.ts
@@ -1,17 +1,41 @@
-import {pool} from "./utils/pool";
 import {UserRecord} from "./records/user.record";
-import {User} from "../types";
+import {Pet, User} from "../types";
+import {PetRecord} from "./records/pet.record";
 
 (async ()=>{
 
-    // const users = await UserRecord.getAll();
-    // console.log(users);
-    //
-    // const newUser:UserRecord = new UserRecord({name:"Tomm",surname:"Smith",email:"TomwwS@gmail.com",password:"Password123",role:'user'})
-    // const result = await newUser.addOne();
-    // console.log(result)
+    // UserRecord
+    const userObj:User = {id:'test',name:"Test",surname:"Test",email:"Test@gmail.com",password:"Test1234",role:'user'};
+    const newUser = new UserRecord(userObj);
+    console.log("New user:",newUser);
 
-    const eo = await UserRecord.getOneByEmail("e@o2.pl");
-    console.log(eo)
+    // await newUser.addOne();
 
+    const allUsers = await UserRecord.getAll();
+    // console.log("All users: ",allUsers);
+
+    const oneUserById = await UserRecord.getOneById('test');
+    // console.log("One user taken by id: ",oneUserById);
+
+    const oneUserByEmail = await UserRecord.getOneByEmail("Test@gmail.com")
+    // console.log("One user taken by e-mail: ",oneUserByEmail);
+
+
+    //PetRecord
+    const petObj:Pet={id:"test",birthday:"2012/12/02",name:"Test",species:"test",race:"test",ownerId:oneUserById.id};
+    const newPet = new PetRecord(petObj);
+    // console.log("New pet:",newPet);
+
+    newPet.name = 'Edited';
+    await newPet.update();
+
+    await newPet.add();
+
+    const allPetsFromOneUser = await PetRecord.getAllFromOneOwner(oneUserById.id);
+    // console.log(allPetsFromOneUser);
+
+    const onePetFromOneUser =await PetRecord.getOneFromOneOwner(oneUserById.id,newPet.id);
+    // console.log(onePetFromOneUser);
+
+    await onePetFromOneUser.delete();
 })()
