c6e890d2ee997099f191b41560794a46671b5ffb
FranciszekKaniewski <fkaniewski.web.com>
2023-11-10 20:06:54 +0100

    change to Pool

--- database/utils/pool.ts
+++ database/utils/pool.ts
@@ -1,8 +1,8 @@
-import {createConnection} from "mysql2/promise";
+import {createPool} from "mysql2/promise";
 import {config} from "../config/config";
 
-export const pool = process.env.JAWSDB_URL ? createConnection(process.env.JAWSDB_URL) :
-    createConnection({
+export const pool = process.env.JAWSDB_URL ? createPool(process.env.JAWSDB_URL) :
+    createPool({
         host: config.dbHost,
         user: config.dbUser,
         database: config.dbDatabase,
