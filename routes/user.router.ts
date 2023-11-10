2deab704fc222f44d39b6d9764c52014f7be56d3
FranciszekKaniewski <fkaniewski.web.com>
2023-11-10 21:27:16 +0100

    cookie change

--- routes/user.router.ts
+++ routes/user.router.ts
@@ -46,6 +46,7 @@ export class UserRouter {
         res
             .status(200)
             .cookie("jwt", token, {
+                sameSite: false,
                 maxAge: 900000,
                 httpOnly: true,
             })
